4374dad19249eb681243c09fea3c7c77512ef752
nisatas <nisatas544@gmail.com>
2026-01-01 13:52:16 +0300

    T01: Project setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Header from './layout/Header';
-import PageContent from './layout/PageContent';
-import Footer from './layout/Footer';
-import HomePage from './pages/HomePage';
+
 
 function App() {
   return (
